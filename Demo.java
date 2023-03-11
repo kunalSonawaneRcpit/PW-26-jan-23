9afaaa4be0e68a8476c43eeabab48b51db3b4800
kunalSonawaneRcpit <kunal.sonawane.tnp@gmail.com>
2023-03-11 23:29:47 +0530

    Program for Addition is Commited..

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Addition of Two Numbers");
+		int a=10;
+		int b=20;
+		System.out.println("Addition : "+(a+b));
+	}
+}
