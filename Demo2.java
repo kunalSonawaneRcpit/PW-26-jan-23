221b21eb6ac694f27046703d7822594233f9ea81
kunalSonawaneRcpit <kunal.sonawane.tnp@gmail.com>
2023-03-12 00:03:25 +0530

    first commit

---
+++ Demo2.java
@@ -0,0 +1,14 @@
+class Demo2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Passing Data from Arguments at Run time through command line and Displaying it");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+}
+
+// Compile and Run -
+// COMPILE ==> D:\PWJava1\Java>javac Demo2.java
+// RUN ======> D:\PWJava1\Java>java Demo2 Kunal Bhagwan Sonawane
